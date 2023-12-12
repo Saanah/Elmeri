2c010762116ffdfda825d8a6e4aecaaf14e07492
Saana <t0pusa00@students.oamk.fi>
2023-12-13 00:43:39 +0200

    Ohjeet poistettu routterista

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Etusivu from "./components/Etusivu";
 import RaporttiHistoria from "./components/RaporttiHistoria";
-import Ohjeet from "./components/Ohjeet";
 import Raportinluonti from './components/Raportinluonti';
 import PdfUpload from "./components/PdfUpload";
 import Tarkastuskohdat from "./components/Tarkastuskohdat";
@@ -13,7 +12,6 @@ function App() {
     <Route path="/" element={<Etusivu/>}></Route>
     <Route path="/raportit" element={<RaporttiHistoria/>}></Route>
     <Route path="/tarkastuskohdat" element={<Tarkastuskohdat/>}></Route>
-    <Route path="/ohjeet" element={<Ohjeet/>}></Route>
     <Route path="/luo_uusi_raportti" element={<Tarkastuskohdat/>}></Route>
   </Routes>
   );
